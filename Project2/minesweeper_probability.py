d92058debc0c889c17e1252410b7f41df7df45cb
TobyGE <15188313009@163.com>
2017-10-28 00:54:47 +0800

    matrix

--- Project2/minesweeper_probability.py
+++ Project2/minesweeper_probability.py
@@ -4,7 +4,9 @@
 import sys
 import MineGenerator
 import Queue
+import collections
 from itertools import product
+
 
 
 class Node:
@@ -17,8 +19,8 @@ class Node:
 class minesweeper_probability(object):
     def __init__(self):
         self.close = dict()   # inner boundary(has clue)
-        self.frontier = dict()  # outer bound
-        self.node_hasbeen_travel = dict()
+        self.frontier = collections.OrderedDict()  # outer bound
+        self.has_been_travelled = dict()
 
     def __minesweeper_init(self, matrix, height, width):
         cell = [0,0]
@@ -31,18 +33,45 @@ class minesweeper_probability(object):
                 break
 
             self.close[cell] = clue
+            self.has_been_travelled[cell] = clue
             self.extend_surround(cell)
-            cell = find_next(self.frontier,self.close)
+            cell = self.find_next(self.frontier,self.close)
             self.frontier.pop(cell)
 
 
-    def find_next(self, frontier):
-        set
+    def find_next(self):
+        length = len(self.frontier)
+        q = Queue.Queue()
+        for i in product(range(-1, 0), repeat=length):
+            q.put(i)
 
-        len = 3
-        for i in product(range(2), repeat=len):
-            print i
+        for (key, value) in self.close:
+            #for every cell in frontier, set its value to possible number
+            combination = q.pop()
+            j = 0
+            for (k,v) in self.frontier:
+                self.frontier[k] = combination[j]
+                j = j + 1
 
+            deltaX = [-1, 0, 1, -1, 1, -1, 0, 1]
+            deltaY = [-1, -1, -1, 0, 0, 1, 1, 1]
+            sum = 0
+            for i in range(0, 8):
+                neighborNode = (self.key[0] + deltaX[i], self.key[1] + deltaY[i])
+                if neighborNode[0] < 0 or neighborNode[1] < 0 or neighborNode[1] \
+                        >= self.size or neighborNode[0] >= self.size:
+                    continue
+                if self.has_been_travelled.has_key(neighborNode):
+                    continue
+                if self.frontier.has_key(neighborNode):
+                    sum += self.frontier.get(neighborNode)
+            if sum != value:
+                break
+
+
+
+
+        next_cell = [1,1]
         return next_cell
 
     # discover the node's surrounding environment, and put into frontal
@@ -51,6 +80,7 @@ class minesweeper_probability(object):
         deltaY = [-1,-1,-1,0,0,1,1,1]
 
         # establish new nodes by changing the given node's coordinates in 4 directions
+        possible = dict()
         for i in range(0, 8):
             neighborNode = (cell[0] + deltaX[i], cell[1] + deltaY[i])
 
@@ -59,10 +89,15 @@ class minesweeper_probability(object):
                     >= self.size or neighborNode[0] >= self.size:
                 continue
 
-            if self.frontier.has_key(neighborNode)
+            if self.has_been_travelled.has_key(neighborNode):
                 continue
 
-            self.frontier[neighborNode]
+            possible[neighborNode] = -1
+
+        if len(possible) ==  self.close.get(cell):
+            self.has_been_travelled = dict(self.has_been_travelled.items() + possible.items())
+        else:
+            self.frontier = dict(self.frontier.items() + possible.items())
 
 
 if __name__ == "__main__":
